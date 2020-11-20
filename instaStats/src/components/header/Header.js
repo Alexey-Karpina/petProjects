be4a63227c930aefe48d21f3b9f637b80873f22b
user <C:\Users\User\AppData\Roaming\The Bat!>
2020-11-20 09:27:52 +0200

    Test

---
+++ instaStats/src/components/header/Header.js
@@ -0,0 +1,48 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import { makeStyles } from '@material-ui/core/styles';
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
+
+function Header() {
+    const classes = useStyles();
+
+  return (
+    <AppBar position="static" className={classes.root}>
+      <Toolbar>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          className={classes.menuButton}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" className={classes.title}>
+          Home
+        </Typography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Header;
+
+
+
